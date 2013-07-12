a3282ed74cffd3938de45c203bd2fcfc20dffc1d
oasisfeng <git@id.oasisfeng.com>
2013-07-12 10:40:23 +0800

    ADD: Alert for Signature mismatch, instead of crash. UPDATE: Minor cleanups.

--- app/src/com/taobao/android/ski/hud/LaunchPad.java
+++ app/src/com/taobao/android/ski/hud/LaunchPad.java
@@ -1,12 +1,11 @@
 package com.taobao.android.ski.hud;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.ComponentName;
 import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.os.Debug;
-import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.EditText;
@@ -29,19 +28,18 @@ public class LaunchPad extends Activity {
 		findViewById(R.id.launch_profiling).setOnClickListener(new OnClickListener() { @Override public void onClick(View v) {
 			start(Dock.FLAG_LAUNCH_PROFILING);
 		}});
-		
+
 		mContext = this;
 	}
 
 	@Override
 	protected void onDestroy() {
 		super.onDestroy();
-		
-		
+
+
 	}
 
 	private void start(int flags) {
-		
 		//activity leak switch
 		ToggleButton actleakbut = (ToggleButton)findViewById(R.id.activityleakbutton);
 		if(actleakbut.isChecked()) {
@@ -53,17 +51,17 @@ public class LaunchPad extends Activity {
 		if(actbut.isChecked()) {
 			flags = flags | Dock.FLAG_MONITOR_ACTIVITY_PERF;
 		}
-		
+
 		//animation perf switch
 		ToggleButton anibut = (ToggleButton)findViewById(R.id.animationperfbutton);
 		if(anibut.isChecked()) {
 			flags = flags | Dock.FLAG_MONITOR_ANIMATION_PERF;
 		}
-		
+
 		Bundle options = new Bundle();
 		options.putInt(Dock.KEY_FLAGS, flags);
 		if (Debug.isDebuggerConnected()) options.putBoolean(Dock.KEY_ATTACH_DEBUGGER, true);
-		
+
 		//am_lanch_time threashold
 		if(actbut.isChecked()) {
 			EditText launch_time = (EditText)findViewById(R.id.launch_time);
@@ -75,7 +73,7 @@ public class LaunchPad extends Activity {
 				options.putInt(Dock.KEY_ACTIVITY_LAUNCH_TIME, 500);
 			}
 		}
-		
+
 		EditText threashold = (EditText)findViewById(R.id.edit_content);
 		String thd = threashold.getText().toString().trim();
 		try {
@@ -84,15 +82,21 @@ public class LaunchPad extends Activity {
 		} catch (NumberFormatException e){
 			options.putInt(Dock.KEY_THREASHOLD, 5);
 		}
-		
+
 		ToggleButton tbut = (ToggleButton)findViewById(R.id.strictmodebutton);
 		boolean result = false;
-		if(tbut.isChecked()) {
-			result = startInstrumentation(new ComponentName(this, DockForStrictMode.class), null, options);
-		} else {
-			result = startInstrumentation(new ComponentName(this, Dock.class), null, options);
+		try {
+			if (tbut.isChecked()) {
+				result = startInstrumentation(new ComponentName(this, DockForStrictMode.class), null, options);
+			} else {
+				result = startInstrumentation(new ComponentName(this, Dock.class), null, options);
+			}
+		} catch (SecurityException e) {
+			new AlertDialog.Builder(mContext).setTitle("Error")
+					.setMessage("Signature mismatch. Please ensure target app is signed with the same certificate as me.")
+					.show();
 		}
-		 
+
 		if (! result) throw new LinkageError("Instrumentation is not correctly configured. May be your target app didn't installed");
 	}
 }
