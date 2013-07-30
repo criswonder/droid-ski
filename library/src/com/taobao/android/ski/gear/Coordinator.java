dbe5cdd98d92c17c906031c695e5aa35af8ed8b5
baiyi.hwj <baiyi.hwj@taobao.com>
2013-07-30 15:01:12 +0800

    ADD：repackage miss TaggedRunnable

--- library/src/com/taobao/android/ski/gear/Coordinator.java
+++ library/src/com/taobao/android/ski/gear/Coordinator.java
@@ -1,4 +1,13 @@
 package com.taobao.android.ski.gear;
 
 /** @deprecated Moved to package com.taobao.android.task */
-@Deprecated public class Coordinator extends com.taobao.android.task.Coordinator {}
+@Deprecated public class Coordinator extends com.taobao.android.task.Coordinator {
+	
+	@Deprecated
+	public static abstract class TaggedRunnable extends com.taobao.android.task.Coordinator.TaggedRunnable {
+
+		public TaggedRunnable(String tag) {
+			super(tag);
+		}
+	}
+}
