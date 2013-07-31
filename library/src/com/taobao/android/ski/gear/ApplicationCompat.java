5fa92bad9384073c867208d194f31adac7a79213
baiyi.hwj <baiyi.hwj@taobao.com>
2013-07-31 16:36:28 +0800

    ADD ： repackage miss class

--- library/src/com/taobao/android/ski/gear/ApplicationCompat.java
+++ library/src/com/taobao/android/ski/gear/ApplicationCompat.java
@@ -1,4 +1,17 @@
 package com.taobao.android.ski.gear;
 
+
+
 /** @deprecated Moved to package com.taobao.android.compat */
-@Deprecated public class ApplicationCompat extends com.taobao.android.compat.ApplicationCompat {}
+@Deprecated public class ApplicationCompat extends com.taobao.android.compat.ApplicationCompat {
+	
+	@Deprecated
+    public interface ActivityLifecycleCallbacksCompat extends com.taobao.android.compat.ApplicationCompat.ActivityLifecycleCallbacksCompat {
+        
+    }
+
+	@Deprecated
+    public static class AbstractActivityLifecycleCallbacks extends com.taobao.android.compat.ApplicationCompat.AbstractActivityLifecycleCallbacks {
+
+    }
+}
--- library/src/com/taobao/android/ski/gear/SkiApplication.java
+++ library/src/com/taobao/android/ski/gear/SkiApplication.java
@@ -1,4 +1,11 @@
 package com.taobao.android.ski.gear;
 
 /** @deprecated Use {@link com.taobao.android.lifecycle.PanguApplication} instead */
-@Deprecated public class SkiApplication extends com.taobao.android.lifecycle.PanguApplication {}
+@Deprecated public class SkiApplication extends com.taobao.android.lifecycle.PanguApplication {
+	
+	@Deprecated
+	public interface CrossActivityLifecycleCallback extends com.taobao.android.lifecycle.PanguApplication.CrossActivityLifecycleCallback {
+		
+	}
+	
+}
