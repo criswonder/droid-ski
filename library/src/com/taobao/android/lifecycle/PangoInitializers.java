77a76976db2ee3ee4b44d15afe81a49ffcd597e7
oasisfeng <git@id.oasisfeng.com>
2013-08-01 20:36:46 +0800

    UPDATE: Refined the design of PangoInitializers.

--- library/src/com/taobao/android/lifecycle/PangoInitializers.java
+++ library/src/com/taobao/android/lifecycle/PangoInitializers.java
@@ -12,6 +12,7 @@ import java.util.Iterator;
 import java.util.List;
 
 import android.app.Activity;
+import android.content.Context;
 import android.util.Log;
 import android.util.SparseArray;
 
@@ -32,30 +33,27 @@ public abstract class PangoInitializers {
 		private static final long serialVersionUID = 1L;
 	}
 
-	/** Annotate a initXXX() method to be run synchronously */
-	@Retention(RetentionPolicy.RUNTIME)
-	@Target(ElementType.METHOD)
-	protected @interface Sync {
-		/** Initializers with lowest priority start first.
-		 *  No particular order for initializers with same priority. */
-		int priority();
+	/** Annotate a initXXX() method to be run synchronously in the specific priority, 0 if absent.
+	 *  Initializers with lowest priority start first. No particular order for same priority. */
+	@Target(ElementType.METHOD) @Retention(RetentionPolicy.RUNTIME)
+	protected @interface Priority {
+		int value();
 	}
 
-	/** Annotate initXXX() method to be run asynchronously */
-	@Retention(RetentionPolicy.RUNTIME)
-	@Target(ElementType.METHOD)
+	/** Annotate initXXX() method to be run asynchronously in no particular order */
+	@Target(ElementType.METHOD) @Retention(RetentionPolicy.RUNTIME)
 	protected @interface Async {}
 
-	/** Annotate initXXX() method to be run asynchronously when the main thread became idle. */
-	@Retention(RetentionPolicy.RUNTIME)
-	@Target(ElementType.METHOD)
+	/** Annotate initXXX() method to be run asynchronously when the main thread became idle,
+	 *  in no particular order */
+	@Target(ElementType.METHOD) @Retention(RetentionPolicy.RUNTIME)
 	protected @interface Delayed {}
 
-	/** Annotate initXXX() method needs to be run only if UI is going to be started. (optional)
-	 *  Can be used together with above annotations */
-	@Retention(RetentionPolicy.RUNTIME)
-	@Target(ElementType.METHOD)
-	protected @interface UiOnly {}
+	/** Annotate initXXX() method which is needed not only by UI, but also by other components
+	 *  (including Service, global BroadcastReceiver, ContentProvider, etc.).
+	 *  (optional, to be used together with above annotations) */
+	@Target(ElementType.METHOD) @Retention(RetentionPolicy.RUNTIME)
+	protected @interface Global {}
 
 	/** Should only be called in initializer method */
 	protected PanguApplication getApplication() {
@@ -66,18 +64,18 @@ public abstract class PangoInitializers {
 
 	public void start(final PanguApplication application) {
 		mApplication = application;
-		
+
 		Coordinator.runTask(new TaggedRunnable("PangoInitializers.parse") { @Override public void run() {
 			parse();
 		}});
 
-		startInitializersWithout(UiOnly.class);
+		startInitializersAnnotatedBy(Global.class);
 
 		application.registerCrossActivityLifecycleCallback(new CrossActivityLifecycleCallback() {
 
 			@Override public void onCreated(Activity activity) {
 				application.unregisterCrossActivityLifecycleCallback(this);
-				startInitializersWithout(null);
+				startInitializersAnnotatedBy(null);
 			}
 
 			@Override public void onStopped(Activity activity) {}
@@ -86,12 +84,12 @@ public abstract class PangoInitializers {
 		});
 	}
 
-	private void startInitializersWithout(Class<? extends Annotation> annotation) {
+	private void startInitializersAnnotatedBy(Class<? extends Annotation> annotation) {
 		// Post asynchronous initializers
 		Iterator<Method> iterator = mAsyncInitializers.iterator();
 		while(iterator.hasNext()) {
 			final Method method = iterator.next();
-			if (annotation != null && method.isAnnotationPresent(annotation)) continue;
+			if (annotation == null || ! method.isAnnotationPresent(annotation)) continue;
 			Coordinator.postTask(new TaggedRunnable(method.getName()) { @Override public void run() {
 				invokeInitializer(method);
 			}});
@@ -123,34 +121,34 @@ public abstract class PangoInitializers {
 	}
 
 	private void parse() {
-		Method[] methods = getClass().getMethods();
+		boolean debug = Versions.isDebug();
+		Method[] methods = getClass().getDeclaredMethods();
 		for (Method method : methods) {
 			String name = method.getName();
 			Log.d("PangoInitializers", getClass().toString() + ":"+ name);
 			if (name.length() < 5 || ! name.startsWith("init")
 					|| ! Character.isUpperCase(name.charAt(4))) continue;
-			if (Versions.isDebug()) {		// Only check qualification in DEBUG build.
-				if ((method.getModifiers() & Modifier.STATIC) == 0)
-					throw new UnqualifiedInitializerError("Non-static: " + name);
+			if (debug) {		// Only check qualification in DEBUG build.
+				if ((method.getModifiers() & Modifier.PRIVATE) != 0)
+					throw new UnqualifiedInitializerError("Private: " + name);
+				if ((method.getModifiers() & Modifier.STATIC) != 0)
+					throw new UnqualifiedInitializerError("Static: " + name);
 				if (method.getParameterTypes().length != 0)
 					throw new UnqualifiedInitializerError("With parameters: " + name);
 				if (method.getReturnType() != Void.class)
 					throw new UnqualifiedInitializerError("Non-void return type: " + name);
 			}
 
-			for (Annotation annotation : method.getAnnotations()) {
-				Class<? extends Annotation> type = annotation.getClass();
-				if (Sync.class.isAssignableFrom(type)) {
-					int priority = ((Sync) annotation).priority();
-					List<Method> initializers = mSyncInitializers.get(priority);
-					if (initializers == null) mSyncInitializers.put(priority, initializers = new ArrayList<Method>());
-					initializers.add(method);
-				} else if (Delayed.class.isAssignableFrom(type)) {
-					mDelayedInitializers.add(method);
-				} else if (Async.class.isAssignableFrom(type)) {
-					mAsyncInitializers.add(method);
-				} else if (Versions.isDebug())
-					throw new UnqualifiedInitializerError("No mandatory annotation: " + name);
+			if (method.isAnnotationPresent(Delayed.class)) {
+				mDelayedInitializers.add(method);
+			} else if (method.isAnnotationPresent(Async.class)) {
+				mAsyncInitializers.add(method);
+			} else {
+				Priority priority_annonation = method.getAnnotation(Priority.class);
+				int priority = priority_annonation == null ? 0 : priority_annonation.value();
+				List<Method> initializers = mSyncInitializers.get(priority);
+				if (initializers == null) mSyncInitializers.put(priority, initializers = new ArrayList<Method>());
+				initializers.add(method);
 			}
 		}
 	}
@@ -169,24 +167,40 @@ public abstract class PangoInitializers {
 	private final List<Method> mDelayedInitializers = new ArrayList<Method>();
 }
 
-class DemoInitializers extends PangoInitializers {
 
-	@Sync(priority=2) @UiOnly
-	public static void initImageManager() {}
+class DemoApplication extends PanguApplication {
 
-	@Delayed
-	public static void initGoogleAnalytics() {}
-
-	@Override
-	public void onInitializerException(Method method, Exception exception) {
-		// Send exception report
-		// Attempt to recovery from exception
-		// Prepare for safe-mode restart
-		// ...
+	@Override public void onCreate() {
+		super.onCreate();
+		new DemoInitializers().start(this);
 	}
 
-	public static void main(String[] args) {
-		PanguApplication application = null;
-		new DemoInitializers().start(application);
+	static class DemoInitializers extends PangoInitializers {
+
+		@Priority(2)
+		public void initImageManager() {}
+
+		@Async
+		public void initXXX() {}
+
+		@Delayed @Global
+		public void initGoogleAnalytics() {
+			@SuppressWarnings("unused")
+			Context context = getApplication().getApplicationContext();
+			// Use context to initialize Google Analytics
+		}
+
+		@Override
+		public void onInitializerException(Method method, Exception exception) {
+			// Send exception report
+			// Attempt to recovery from exception
+			// Prepare for safe-mode restart
+			// ...
+		}
+
+		public static void main(String[] args) {
+			PanguApplication application = null;
+			new DemoInitializers().start(application);
+		}
 	}
 }
