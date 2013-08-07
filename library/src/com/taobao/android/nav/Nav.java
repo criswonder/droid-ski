aff9964ffc32e95dfe09ae43e2c14add652dc2c0
oasisfeng <git@id.oasisfeng.com>
2013-08-07 11:45:04 +0800

    ADD: Preprocessor mechanism in Nav.

--- library/src/com/taobao/android/nav/Nav.java
+++ library/src/com/taobao/android/nav/Nav.java
@@ -1,5 +1,8 @@
 package com.taobao.android.nav;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
@@ -16,6 +19,7 @@ import android.os.Bundle;
  */
 public class Nav {
 
+	/** @param context use current Activity if possible */
 	public static Nav from(Context context) {
 		return new Nav(context);
 	}
@@ -25,10 +29,30 @@ public class Nav {
 		return this;
 	}
 
+	/**
+	 *  <b>Intent returned by this method should NEVER be kept</b>, but used in
+	 *  {@link android.content.Context#startActivity(Intent) Context.startActivity()} or its variants.
+	 */
 	public Intent to(Uri uri) {
+		if (! mPreprocessor.isEmpty()) {
+			for (NavPreprocessor preprocessor : mPreprocessor)
+				uri = preprocessor.beforeNavToUri(uri);
+		}
 		Intent intent = new Intent(Intent.ACTION_VIEW, uri).setPackage(mContext.getPackageName());
 		if (mExtras != null) intent.putExtras(mExtras);
 		return intent;
+	}
+
+	public static interface NavPreprocessor {
+		Uri beforeNavToUri(Uri uri);
+	}
+
+	public static void registerPreprocessor(NavPreprocessor preprocessor) {
+		mPreprocessor.add(preprocessor);
+	}
+
+	public static void unregisterPreprocessor(NavPreprocessor preprocessor) {
+		mPreprocessor.remove(preprocessor);
 	}
 
 	private Nav(Context context) {
@@ -37,6 +61,7 @@ public class Nav {
 
 	private final Context mContext;
 	private Bundle mExtras;
+	private static final List<NavPreprocessor> mPreprocessor = new ArrayList<Nav.NavPreprocessor>();
 
 	/** Demonstrate the usage of {@link Nav} */
 	static class DemoActivity extends Activity {
