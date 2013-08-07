902387404f30ccd8cb743ec896a1c869ba671d20
baiyi.hwj <baiyi.hwj@taobao.com>
2013-08-07 19:39:22 +0800

    CHANGE：actiivty leak monitor parameter

--- app/src/com/taobao/android/ski/radar/ActivityLeakMonitor.java
+++ app/src/com/taobao/android/ski/radar/ActivityLeakMonitor.java
@@ -3,14 +3,9 @@ package com.taobao.android.ski.radar;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.lang.ref.ReferenceQueue;
 import java.lang.ref.WeakReference;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.ListIterator;
 
 import android.app.Activity;
 import android.content.Context;
@@ -27,7 +22,7 @@ public class ActivityLeakMonitor {
 	
 	private final static String TAG = "ActivityLeakMonitor";
 
-	private final static int ACTIVITY_MONITOR_INTVL = 15000;
+	private final static int ACTIVITY_MONITOR_INTVL = 20000;
 		
 	private static Context mAppContext;
 	private static Handler mHandler;
@@ -92,10 +87,34 @@ public class ActivityLeakMonitor {
 		public void run() {
 			String state = Environment.getExternalStorageState();
 	        if (state != null && state.equals(android.os.Environment.MEDIA_MOUNTED)){
-	        	String sdcard = Environment.getExternalStorageDirectory().toString();
-	        	File fileDir = new File(sdcard,"MemoryMonitor/"+mAppContext.getPackageName()+"/leak_activity/"+ mTimeForName + "/" + leakActivity);
-	        	fileDir.mkdirs();
 	        	
+	        	String sdcard = Environment.getExternalStorageDirectory().toString();
+	        	File fileDir = new File(sdcard,"MemoryMointor/"+ mAppContext.getPackageName() + "/leak_activity/"+ mTimeForName);
+	        	fileDir.mkdirs();
+	        	File leakList = new File(fileDir.getAbsolutePath(),"ActivityList.txt");
+	        	FileOutputStream os = null;
+	        	try {
+	        		if(!leakList.exists())
+	        			leakList.createNewFile();
+					os = new FileOutputStream(leakList);
+					StringBuffer str = new StringBuffer();
+					
+					str.append("list:\r\n");
+					str.append(leakActivity+"\r\n");
+					os.write(str.toString().getBytes());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}finally{
+					if(os != null) {
+						try {
+							os.close();
+						} catch (IOException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+					}
+				}
+
 	        	try {
 	        		File hprofFile = new File(fileDir.getAbsolutePath(),"com.taobao.taobao.hprof");
 	        		hprofFile.delete();
