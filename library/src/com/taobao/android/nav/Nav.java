15d94720ccba03bba51234682f17ac56d6aff537
oasisfeng <git@id.oasisfeng.com>
2013-08-27 17:16:23 +0800

    UPDATE: Redesigned the Nav APIs.

--- library/src/com/taobao/android/nav/Nav.java
+++ library/src/com/taobao/android/nav/Nav.java
@@ -3,9 +3,11 @@ package com.taobao.android.nav;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.NonNullByDefault;
 import javax.annotation.Nullable;
 
 import android.app.Activity;
+import android.content.ActivityNotFoundException;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
@@ -13,18 +15,21 @@ import android.net.Uri;
 import android.os.Bundle;
 
 /**
- * Navigation by URL
+ * Cross-Platform Navigation (based on URI)
  *
  * <p><b>Instance should NEVER be kept, due to possible memory-leak to context.</b></p>
  *
  *  @see DemoActivity
  *  @author Oasis
  */
+@NonNullByDefault
 public class Nav {
 
 	public static final String KExtraReferrer = "referrer";
 
+	/** Preprocessor for every navigation attempts */
 	public static interface NavPreprocessor {
+
 		/**
 		 * Check the intent and make changes if needed.
 		 *
@@ -33,36 +38,85 @@ public class Nav {
 		boolean beforeNavTo(Intent intent);
 	}
 
+	/** Exception handler to be triggered for navigation exception. */
+	public static interface NavExceptionHandler {
+
+		/**
+		 * Called when failed to navigate to the specific destination.
+		 *
+		 * @param intent the activity intent to be started which caused the exception.
+		 *   It is expected to be changed if retry is needed.
+		 * @param e the exception (most probably {@link android.content.ActivityNotFoundException ActivityNotFoundException})
+		 * @return whether to retry the navigation. <b>When failed in retry, this called will not be called again.</b>
+		 */
+		boolean onException(Intent intent, Exception e);
+	}
+
 	/** @param context use current Activity if possible */
 	public static Nav from(final Context context) {
 		return new Nav(context);
 	}
 
-	/**
-	 *  <b>Intent returned by this method should NEVER be kept</b>, but used immediately in
-	 *  {@link android.content.Context#startActivity(Intent)} or its variants.
-	 */
-	public @Nullable Intent to(final Uri uri) {
-		final Intent intent = new Intent(Intent.ACTION_VIEW, uri).setPackage(mContext.getPackageName());
+	/** Allow navigation to escape current application (for 3rd-party activity) */
+	public Nav allowEscape() {
+		mAllowLeaving = true;
+		return this;
+	}
+
+	/** Extras to be put into activity intent. */
+	public Nav withExtras(final Bundle extras) {
+		mIntent.putExtras(extras);
+		return this;
+	}
+
+	/** Flags to be added to activity intent */
+	public Nav withFlags(final int flags) {
+		mIntent.addFlags(flags);
+		return this;
+	}
+
+	/** Start activity associated with the specific URI. */
+	public void toUri(final Uri uri) {
+		final Intent intent = to(uri);
+		if (intent == null) return;
+		NavExceptionHandler exception_handler = mExceptionHandler;
+		for (;;) try {
+			mContext.startActivity(intent);
+			break;
+		} catch (final ActivityNotFoundException e) {
+			if (exception_handler != null && exception_handler.onException(intent, e)) {
+				exception_handler = null;		// To avoid dead-loop.
+				continue;
+			}
+			break;
+		}
+	}
+
+	/** @deprecated Use {@link #toUri(Uri uri)} instead. */
+	@Deprecated public @Nullable Intent to(final Uri uri) {
+		mIntent.setData(uri);
+		if (! mAllowLeaving) mIntent.setPackage(mContext.getPackageName());
+		// Add referrer extra
 		if (mContext instanceof Activity) {
 			final Intent from_intent = ((Activity) mContext).getIntent();
 			if (from_intent != null) {
 				final Uri referrer_uri = from_intent.getData(); ComponentName comp;
-				if (referrer_uri != null) intent.putExtra(KExtraReferrer, referrer_uri.toString());
+				if (referrer_uri != null) mIntent.putExtra(KExtraReferrer, referrer_uri.toString());
 				else if ((comp = from_intent.getComponent()) != null)		// Compact (component only)
-					intent.putExtra(KExtraReferrer, new Intent().setComponent(comp).toUri(0));
-				else intent.putExtra(KExtraReferrer, from_intent.toUri(0));	// Legacy
+					mIntent.putExtra(KExtraReferrer, new Intent().setComponent(comp).toUri(0));
+				else mIntent.putExtra(KExtraReferrer, from_intent.toUri(0));	// Legacy
 			}
 		}
-		if (mExtras != null) intent.putExtras(mExtras);
+		// Run preprocessors
 		if (! mPreprocessor.isEmpty())
 			for (final NavPreprocessor preprocessor : mPreprocessor)
-				if (! preprocessor.beforeNavTo(intent))
+				if (! preprocessor.beforeNavTo(mIntent))
 					return null;
-		return intent;
+		return mIntent;
 	}
 
-	public @Nullable Intent toActivity(final Uri uri) {
+	/** @deprecated Use {@link #toUri(Uri uri)} instead. */
+	@Deprecated public @Nullable Intent toActivity(final Uri uri) {
 		final Intent intent = to(uri);
 		if (intent == null) return null;
 		final ComponentName component = intent.resolveActivity(mContext.getPackageManager());
@@ -78,20 +132,29 @@ public class Nav {
 		mPreprocessor.remove(preprocessor);
 	}
 
+	public static void setExceptionHandler(final NavExceptionHandler handler) {
+		mExceptionHandler = handler;
+	}
+
 	private Nav(final Context context) {
 		mContext = context;
+		mIntent = new Intent(Intent.ACTION_VIEW);
 	}
 
 	private final Context mContext;
-	private Bundle mExtras;
-	private static final List<NavPreprocessor> mPreprocessor = new ArrayList<Nav.NavPreprocessor>();
+	private final Intent mIntent;
+	private boolean mAllowLeaving;
+
+	private static final List<NavPreprocessor> mPreprocessor = new ArrayList<NavPreprocessor>();
+	private static @Nullable NavExceptionHandler mExceptionHandler;
 }
 
 /** Demonstrate the usage of {@link Nav} */
+@NonNullByDefault
 class DemoActivity extends Activity {
 
 	void openUri(final Uri uri) {
-		startActivity(Nav.from(this).to(uri));
+		Nav.from(this).toUri(uri);
 	}
 
 	void openUriWithinWebview(final Uri uri) {
