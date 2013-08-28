f3739bbd04dc32662752282275a1d691c56b0b1c
baiyi.hwj <baiyi.hwj@taobao.com>
2013-08-28 17:26:07 +0800

    REMOVE: MTL Mode

--- library/src/com/taobao/android/base/Versions.java
+++ library/src/com/taobao/android/base/Versions.java
@@ -2,17 +2,12 @@ package com.taobao.android.base;
 
 import static android.content.pm.ApplicationInfo.FLAG_DEBUGGABLE;
 
-import java.io.File;
-
 import javax.annotation.NonNullByDefault;
 
 import android.app.Application;
 import android.content.pm.ApplicationInfo;
-import android.os.Environment;
 
 import com.taobao.android.ski.BuildConfig;
-import com.taobao.android.task.Coordinator;
-import com.taobao.android.task.Coordinator.TaggedRunnable;
 
 /** @author Oasis */
 @NonNullByDefault
@@ -21,33 +16,10 @@ public class Versions {
 	public static boolean isDebug() {
 		return DEBUG;
 	}
-	
-	/** MTL Test Mode, this mode will logcat some performace info*/
-	public static boolean isMTLMode(){
-		return IOTMODE;
-	}
-	
-	/** Get current process name*/
-	public static String getCurrentProcessName() {
-		return PROCESSNAME;
-	}
 
     /** Call this method in Application.onCreate() */
     public static void init(final Application application) {
     	
-    	Coordinator.runTasks(new TaggedRunnable("CheckMTLMode") { @Override public void run() {
-    		StringBuilder sb = (new StringBuilder()).append(
-    				Environment.getExternalStorageDirectory().toString())
-    				.append(File.separator).append("MTL_IOT");
-        	
-    		File file = new File(sb.toString());
-        	if(file.exists())
-        		IOTMODE = true;
-        	
-        	PROCESSNAME = Tools.currentProcessName(application.getApplicationContext());
-		}});
-
-  	
         if (! DEBUG) return;
         // To workaround the unreliable "BuildConfig.DEBUG".
         //   See http://code.google.com/p/android/issues/detail?id=27940
@@ -58,8 +30,5 @@ public class Versions {
     }
 
     private static boolean DEBUG = BuildConfig.DEBUG;
-    
-    private static boolean IOTMODE = false;
-    
-    private static String PROCESSNAME = null;
+
 }
