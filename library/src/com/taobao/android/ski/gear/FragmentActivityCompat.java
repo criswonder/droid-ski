e191a3b7c41e3816fff0ce13485bc0b739b68892
wufeng <wufeng@taobao.com>
2013-11-16 15:57:12 +0800

    REMOVE: Deprecated code after several iterations.

--- library/src/com/taobao/android/ski/gear/FragmentActivityCompat.java
+++
@@ -1,6 +0,0 @@
-package com.taobao.android.ski.gear;
-
-import javax.annotation.NonNullByDefault;
-
-/** @deprecated Moved to package com.taobao.android.compat */
-@Deprecated @NonNullByDefault public class FragmentActivityCompat extends com.taobao.android.compat.FragmentActivityCompat {}
--- library/src/com/taobao/android/ski/gear/SaturativeExecutor.java
+++
@@ -1,6 +0,0 @@
-package com.taobao.android.ski.gear;
-
-import javax.annotation.NonNullByDefault;
-
-/** @deprecated Moved to package com.taobao.android.task */
-@Deprecated @NonNullByDefault public class SaturativeExecutor extends com.taobao.android.task.SaturativeExecutor {}
